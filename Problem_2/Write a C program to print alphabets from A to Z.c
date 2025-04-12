b42c63d268ac5ef4d21cd86899e271dd506050ff
Md-Saharia-Nafiz <nafizmdsaharia48@gmail.com>
2025-04-12 17:31:34 +0600

    first commit

---
+++ Problem_2/Write a C program to print alphabets from A to Z.c
@@ -0,0 +1,16 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+
+    printf("Alphabets from a - z are: \n");
+    for(ch='A'; ch<='Z'; ch++)
+    {
+        printf("%c\n", ch);
+    }
+
+    return 0;
+}
